60e6a88359456634142105ef950156b44136a3f4
woori <woori.kim.se@gmail.com>
2023-06-06 16:33:47 +0900

    bugfix

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ let dogScore = 0;
 let catScore = 0;
 let isStart = false;
 let ball, cat, dog, background;
-
+let isPowerMode = false;
 let ballSpeedX = 0;
 let ballSpeedY = 0;
-
+let isButtonPressed = false;
 let backUpBallSpeedX = 0;
 let backUpBallSpeedY = 0;
 
@@ -24,9 +24,9 @@ let isMovingDown = false;
 let isMovingLeft = false;
 let isMovingRight = false;
 let isCollisionCooldown = false;
-let difficulty = 1;
+let difficulty = 0;
 let lastCollisionTime = 0;
-const collisionCooldownDuration = 800;
+const collisionCooldownDuration = 400;
 
 const upBtn = document.querySelector(".up-button");
 const leftBtn = document.querySelector(".left-button");
@@ -38,6 +38,7 @@ const difficultyBtn = document.querySelector(".difficulty-btn");
 const musicBtn = document.querySelector(".music-btn");
 const keyDown = {};
 const audio = new Audio("music/bgm.mp3");
+const powerBtn = document.querySelector(".power-btn");
 let isMusicPlaying = false;
 
 const playMusic = () => {
@@ -105,19 +106,35 @@ const ballCollision = (objectX, objectY, objectSize) => {
     return false; // 충돌 없음
   }
 };
-
 const handleCollision = (centerX, centerY) => {
-  const ballCenterX = ballX + 20;
-  const ballCenterY = ballY + 20;
+  const ballCenterX = ballX + ballSize / 2;
+  const ballCenterY = ballY + ballSize / 2;
 
-  const deltaX = centerX - ballCenterX;
-  const deltaY = centerY - ballCenterY;
+  const deltaX = ballCenterX - centerX;
+  const deltaY = ballCenterY - centerY;
 
-  ballSpeedX = Math.random() < 0.5 ? 2 : -2;
-  ballSpeedY = Math.random() < 0.5 ? 2 : -2;
+  const angle = Math.atan2(deltaY, deltaX); // 공과 충돌하는 대상 사이의 각도를 계산합니다.
 
-  ballSpeedX = Math.sign(deltaX) * Math.abs(ballSpeedX);
-  ballSpeedY = Math.sign(deltaY) * Math.abs(ballSpeedY);
+  const speed = Math.sqrt(ballSpeedX * ballSpeedX + ballSpeedY * ballSpeedY); // 공의 현재 속도를 계산합니다.
+  
+  const collisionFactor = 1.5;
+  isCollisionCooldown = true;
+  if (centerX < ballX) {
+    ballX =+ 10
+  } 
+
+  if (centerY < ballY) {
+    ballY =+10;
+  }
+  setTimeout(() => {
+    isCollisionCooldown = false;
+  }, collisionCooldownDuration);
+
+
+  // 새로운 속도를 X축과 Y축 속도로 변환합니다.
+  ballSpeedX = (Math.cos(angle) * speed) * collisionFactor;
+  ballSpeedY = (Math.sin(angle) * speed ) * collisionFactor;
+
 };
 
 const handleAnimalCollision = (centerX, centerY) => {
@@ -125,11 +142,31 @@ const handleAnimalCollision = (centerX, centerY) => {
   const animalCenterY = centerY + 30;
 
   handleCollision(animalCenterX, animalCenterY);
+
+  if (centerX < ballX) {
+    if (ballSpeedX < 0) {
+      ballSpeedX = -ballSpeedX;
+    }
+  } else {
+    if (ballSpeedX > 0) {
+      ballSpeedX = -ballSpeedX;
+    }
+  }
+
+  if (centerY < ballY) {
+    if (ballSpeedY < 0) {
+      ballSpeedY = -ballSpeedY;
+    }
+  } else {
+    if (ballSpeedY > 0) {
+      ballSpeedY = -ballSpeedY;
+    }
+  }
 };
 
 const getAnimalCenter = (x, y) => {
-  const centerX = x + 30;
-  const centerY = y + 30;
+  const centerX = x + animalSize / 2;
+  const centerY = y + animalSize / 2;
   return { centerX, centerY };
 };
 
@@ -142,6 +179,29 @@ let touchArrow = {
 };
 
 let isGamePaused = false; // 게임 일시정지 상태를 나타내는 변수
+const drawBall = () => {
+  ctx.beginPath();
+  ctx.arc(ballX, ballY, ballSize / 2, 0, Math.PI * 2);
+  if (isButtonPressed && isPowerMode) {
+    ctx.fillStyle = "red"; // 버튼을 누르거나 파워 모드일 때 빨간색으로 설정
+  } else {
+    ctx.fillStyle = "black"; // 그 외의 경우에는 검은색으로 설정
+  }
+  ctx.fill();
+  ctx.closePath();
+};
+
+const increaseBallSpeed = () => {
+  if (isPowerMode) {
+    const powerFactor = 2; // 공의 속도를 빠르게 하는 요소
+
+    ballSpeedX *= powerFactor;
+    ballSpeedY *= powerFactor;
+    drawBall();
+    // 이펙트를 추가할 수 있습니다. 예를 들어, 공의 색상을 변경하거나 특정 이미지로 대체하는 등의 작업을 수행할 수 있습니다.
+    // 이펙트에 대한 구체적인 내용은 원하는 시각적인 효과에 따라 다를 수 있습니다.
+  }
+};
 
 const stopGameLoop = () => {
   cancelAnimationFrame(gameLoopId);
@@ -186,17 +246,35 @@ const setupKeyboard = () => {
 
   difficultyBtn.addEventListener("mouseup", function () {
     difficulty++;
-    if (difficulty > 3) {
-      difficulty = 1;
+
+    ballSpeedX++;
+    if (difficulty > 2) {
+      difficulty = 0;
+      ballSpeedX = 2;
     }
   });
   difficultyBtn.addEventListener("touchend", function () {
     difficulty++;
-    if (difficulty > 3) {
-      difficulty = 1;
+
+    ballSpeedX++;
+  
+    if (difficulty > 2) {
+      difficulty = 0;
+
+      ballSpeedX = 2;
     }
   });
+  difficultyBtn.addEventListener("click", function () {
+    difficulty++;
 
+    ballSpeedX++;
+  
+    if (difficulty > 2) {
+      difficulty = 0;
+
+      ballSpeedX = 2;
+    }
+  });
   resetBtn.addEventListener("mousedown", function () {
     dogScore = 0;
     catScore = 0;
@@ -306,8 +384,37 @@ const setupKeyboard = () => {
       playMusic();
     }
   });
+  // 버튼을 누를 때와 클릭할 때의 이벤트 처리
+  powerBtn.addEventListener("mousedown", () => {
+    isButtonPressed = true;
+    ballColor = "red"; // 버튼을 누르는 동안 볼의 색상을 빨간색으로 설정
+  });
+
+  powerBtn.addEventListener("click", () => {
+    isButtonPressed = true;
+    ballColor = "red"; // 버튼을 클릭하는 동안 볼의 색상을 빨간색으로 설정
+  });
+
+  // 버튼을 떼었을 때의 이벤트 처리
+  powerBtn.addEventListener("mouseup", () => {
+    isButtonPressed = false;
+    ballColor = "black"; // 버튼을 떼면 볼의 색상을 초기 색상으로 설정
+  });
 };
+
+const checkCollision = () => {
+  if (ballCollision(dogWidth, dogHeight, animalSize)) {
+    const { centerX, centerY } = getAnimalCenter(dogWidth, dogHeight);
+    handleAnimalCollision(centerX, centerY);
+  } 
+  if (ballCollision(catX, catY, animalSize)) {
+    const { centerX, centerY } = getAnimalCenter(catX, catY);
+    handleAnimalCollision(centerX, centerY);
+  }
+};
+
 const update = () => {
+  checkCollision()
   if (39 in keyDown) dogWidth += 5; // 오른쪽 방향키
   if (37 in keyDown) dogWidth -= 5; // 왼쪽 방향키
   if (38 in keyDown) dogHeight -= 5; // 위쪽 방향키
@@ -327,6 +434,18 @@ const update = () => {
   if (isMovingRight && dogWidth + animalSize < canvas.width / 2) {
     dogWidth += 5;
   }
+  if (ballY < 0) {
+    ballY = 0;
+  } else if (ballY + ballSize > canvas.height) {
+    ballY = canvas.height - ballSize;
+  }
+
+  if (ballX < 0) {
+    ballX = 0;
+  } else if (ballX + ballSize > canvas.width) {
+    ballX = canvas.width - ballSize;
+  }
+
   if (isCollisionCooldown) {
     const currentTime = Date.now();
     const elapsed = currentTime - lastCollisionTime;
@@ -338,13 +457,20 @@ const update = () => {
     if (ballCollision(dogWidth, dogHeight, animalSize)) {
       const { centerX, centerY } = getAnimalCenter(dogWidth, dogHeight);
       handleAnimalCollision(centerX, centerY);
+      console.log('ssssddd')
       isCollisionCooldown = true;
+      dogWidth -=10;
+      dogHeight -=10;
       lastCollisionTime = Date.now();
-    } else if (ballCollision(catX, catY, animalSize)) {
+    } 
+    if (ballCollision(catX, catY, animalSize)) {
       const { centerX, centerY } = getAnimalCenter(catX, catY);
       handleAnimalCollision(centerX, centerY);
       isCollisionCooldown = true;
+      catX +=10;
+      catY +=10;
       lastCollisionTime = Date.now();
+      console.log('ca')
     }
   }
 
@@ -353,14 +479,14 @@ const update = () => {
   ballX -= ballSpeedX;
   ballY += ballSpeedY;
 
-  if (ballY <= 0 || ballY >= canvas.height - 40) {
+  if (ballY <= ballSize || ballY >= canvas.height - ballSize) {
     ballSpeedY = -ballSpeedY;
   }
 
-  if (ballX <= 0) {
+  if (ballX <= ballSize) {
     catScore += 5;
     resetPositions();
-  } else if (ballX >= canvas.width - 40) {
+  } else if (ballX >= canvas.width - ballSize) {
     dogScore += 5;
     resetPositions();
   }
@@ -378,8 +504,8 @@ const catMovement = () => {
   const dy = targetY - catY;
 
   const distance = Math.sqrt(dx * dx + dy * dy);
-  const speed = Math.min(distance, difficulty + 1);
-
+  const speed = Math.min(distance, difficulty * 2 + 1);
+  
   const vx = (speed * dx) / distance;
   const vy = (speed * dy) / distance;
 
@@ -405,11 +531,7 @@ const catMovement = () => {
 const drawDifficulty = () => {
   ctx.fillStyle = "black";
   ctx.font = "15px Arial";
-  ctx.fillText(
-    "Difficulty: " + ["Easy", "Medium", "Hard"][difficulty + 1],
-    10,
-    40
-  );
+  ctx.fillText("Difficulty: " + ["Easy", "Medium", "Hard"][difficulty], 10, 40);
 };
 
 const render = () => {
